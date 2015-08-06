6bd96411b2244a14e0adcddaf2c41e270ba5a89d
梁志聪 <liangzhicong@ppmoney.com>
2015-08-06 09:53:25 +0800

    添加快排

--- release/src/main/java/com/sin90lzc/java/util/SortUtils.java
+++ release/src/main/java/com/sin90lzc/java/util/SortUtils.java
@@ -4,6 +4,7 @@
 package com.sin90lzc.java.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -20,43 +21,31 @@ public class SortUtils {
 	public static final SortDirect ASC=SortDirect.ASC;
 	public static final SortDirect DESC=SortDirect.DESC;
 	
-	private static <T extends Comparable> List<T> sortByMaoPao(List<T> src,SortDirect direct,boolean isNew) {
+	private static <T extends Comparable> void sortByMaoPao(List<T> src,SortDirect direct) {
 		
-		List<T> dest=null;
-		if(isNew){
-			if(src==null || src.size()<1){
-				return new ArrayList<T>();
-			}else{
-				dest=new ArrayList<T>(src);
-			}
-		}else{
-			dest=src;
-		}
 		if(direct==ASC){
-			for(int i=0;i<dest.size()-1;i++){
-				for(int j=dest.size()-1;j>=1;j--){
-					T last=dest.get(j);
-					T pre=dest.get(j-1);
+			for(int i=0;i<src.size()-1;i++){
+				for(int j=src.size()-1;j>=1;j--){
+					T last=src.get(j);
+					T pre=src.get(j-1);
 					if(last.compareTo(pre)<0){
-						dest.set(j-1, last);
-						dest.set(j,pre);
+						src.set(j-1, last);
+						src.set(j,pre);
 					}
 				}
 			}
 		}else{
-			for(int i=0;i<dest.size()-1;i++){
-				for(int j=dest.size()-1;j>=1;j--){
-					T last=dest.get(j);
-					T pre=dest.get(j-1);
+			for(int i=0;i<src.size()-1;i++){
+				for(int j=src.size()-1;j>=1;j--){
+					T last=src.get(j);
+					T pre=src.get(j-1);
 					if(last.compareTo(pre)>0){
-						dest.set(j-1, last);
-						dest.set(j,pre);
+						src.set(j-1, last);
+						src.set(j,pre);
 					}
 				}
 			}
 		}
-		
-		return dest;
 	}
 	
 	/**
@@ -243,18 +232,67 @@ public class SortUtils {
 		}
 	}
 	
+	private static <K,T extends Comparable<K>> void sortByKuaiPai(List<T> src,SortDirect direct){
+		
+	}
+	
+	private static <K,T extends Comparable<K>> void sortByKuaiPai(T[] src,SortDirect direct){
+		
+	}
+	
+	private static void sortByKuaiPai(int[] src,SortDirect direct){
+		if(src==null || src.length<1){
+			return;
+		}
+		sortByKuaiPai(src, 0, src.length-1, direct);
+	}
+	
+	private static void sortByKuaiPai(int[] src,int startPoint,int endPoint,SortDirect direct){
+		int base=src[startPoint];
+		int leftPoint=0;
+		int rightPoint=endPoint;
+		boolean isLeft=false;
+		while(leftPoint!=rightPoint&&leftPoint<endPoint&&rightPoint>startPoint){
+			if(src[rightPoint]<base&&!isLeft){
+				src[leftPoint]=src[rightPoint];
+				isLeft=true;
+			}else{
+				rightPoint--;
+			}
+			if(src[leftPoint]>base&&isLeft){
+				src[rightPoint]=src[leftPoint];
+				isLeft=false;
+			}else{
+				leftPoint++;
+			}
+		}
+		if(startPoint!=leftPoint)
+			sortByKuaiPai(src,startPoint,leftPoint,direct);
+		if(endPoint!=rightPoint)
+			sortByKuaiPai(src,rightPoint,endPoint,direct);
+	}
+	
+	private static void sortByKuaiPai(long[] src,SortDirect direct){
+		
+	}
+	private static void sortByKuaiPai(float[] src,SortDirect direct){
+		
+	}
+	private static void sortByKuaiPai(double[] src,SortDirect direct){
+		
+	}
 	public static void main(String[] args) {
-//		double[] arr=new double[]{8,0,1,4,3,5,8};
-		List<Integer> list=new ArrayList<Integer>();
-		list.add(8);
-		list.add(0);
-		list.add(1);
-		list.add(4);
-		list.add(3);
-		list.add(5);
-		list.add(8);
-		List<Integer> abc=	sortByMaoPao(list,DESC,true);
-		for(Integer a:list){
+		int[] arr=new int[]{8,0,1,4,3,5,8};
+//		List<Integer> list=new ArrayList<Integer>();
+//		list.add(8);
+//		list.add(0);
+//		list.add(1);
+//		list.add(4);
+//		list.add(3);
+//		list.add(5);
+//		list.add(8);
+		sortByKuaiPai(arr,ASC);
+		for(Integer a:arr){
 			System.out.print(a);
 			System.out.print(",");
 		}
