bb4337c551d3eed83e98c5f47311d00362327d0a
Tim Leung <lzcgame@126.com>
2016-01-16 21:51:26 +0800

    nil

--- mvc_zero_config/doc/cors/js/cors.js
+++ mvc_zero_config/doc/cors/js/cors.js
@@ -3,11 +3,9 @@ $(document).ready(function(){
 	$.ajax(
 	{
 		type:'POST',
-		url:'http://192.168.1.254/cors/getData',
-		headers:{'ContentType':'application/json'},
-		success:function(resp){
-			console.log(resp)
-			$("#resp").text(resp.name);
+		url:'http://192.168.1.14:8080/mc/cors/getData',
+		complete:function(xhr,code){
+			$("#resp").text(xhr.responseText);
 		}
 	}
 	);
