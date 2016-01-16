0e0aaa4b1a5f316d54cad4b3c5606b4eb924ec23
Tim Leung <lzcgame@126.com>
2016-01-16 21:53:51 +0800

    支持cors跨域，但不支持options请求

--- mvc_zero_config/doc/cors/js/cors.js
+++ mvc_zero_config/doc/cors/js/cors.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
 	$.ajax(
 	{
 		type:'POST',
-		url:'http://192.168.1.254/cors/getData',
-		headers:{'ContentType':'application/json'},
-		success:function(resp){
-			console.log(resp)
-			$("#resp").text(resp.name);
+		url:'http://192.168.1.14:8080/mc/cors/getData',
+//		headers:{'ContentType':'application/json'},
+		complete:function(xhr,code){
+			$("#resp").text(xhr.responseText);
 		}
 	}
 	);
