866b9bc5aac59b36e12e1db39329ffbfb9cb8536
梁志聪 <liangzhicong@ppmoney.com>
2016-01-18 17:59:18 +0800

    改成公司的ip地址

--- mvc_zero_config/doc/cors/js/cors.js
+++ mvc_zero_config/doc/cors/js/cors.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$.ajax(
 	{
 		type:'POST',
-		url:'http://192.168.1.14:8080/mc/cors/getData',
+		url:'http://192.168.100.190:8080/mc/cors/getData',
 //		headers:{'ContentType':'application/json'},
 		complete:function(xhr,code){
 			$("#resp").text(xhr.responseText);
